a9424b6a323c6413240f5e65495caa825727a66a
PureAlpha-Sourav <sourav@purealphaventures.com>
2024-01-25 17:16:49 +0530

    Added Correct first TP and SL

--- config.py
+++ config.py
@@ -7,8 +7,8 @@
 #     }
     
 thread_H4_Jan24_Signal_EURUSD_GBPUSD_NZDUSD_BySourav = {
-        "Short_H4_1" : ('GBPUSD' , 0.002 , 0.0001 , 3 , 5 , 10 , [1,3,4,5] , ['Trail', 'Trail', 'Trail', 'Trail']),
-        "Short_H4_2" : ('EURUSD' , 0.002 , 0.0001 , 3 , 5 , 10 , [1,3,4,5] , ['Trail', 'Trail', 'Trail', 'Trail']),
-        "Short_H4_3" : ('NZDUSD' , 0.002 , 0.0001 , 3 , 5 , 10 , [1,3,4,5] , ['Trail', 'Trail', 'Trail', 'Trail'])
+        "Short_H4_1" : ('GBPUSD' , 0.002 , 0.0001 , 30 , 50 , 10 , [1,3,4,5] , ['Trail', 'Trail', 'Trail', 'Trail']),
+        "Short_H4_2" : ('EURUSD' , 0.002 , 0.0001 , 30 , 50 , 10 , [1,3,4,5] , ['Trail', 'Trail', 'Trail', 'Trail']),
+        "Short_H4_3" : ('NZDUSD' , 0.002 , 0.0001 , 30 , 50 , 10 , [1,3,4,5] , ['Trail', 'Trail', 'Trail', 'Trail'])
     }
 
