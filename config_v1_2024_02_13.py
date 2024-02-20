8178c9841981df7f6e05792a577a5bb60ef748b0
PureAlpha-Sourav <sourav@purealphaventures.com>
2024-02-20 15:38:52 +0530

    Fixed Bugs

--- config_v1_2024_02_13.py
+++ config_v1_2024_02_13.py
@@ -12,9 +12,9 @@
 #         "Short_H4_7" : ('NZDUSD' , 1/100 , 300 , 500 , 10 , [1] , ['ATR']) ,
 #     }
 thread_H4_Jan24_Signal_EURUSD_GBPUSD_NZDUSD_BySourav = {
-        "Short_H4_2" : ('XAUUSD' , 1/100 , 30 , 50 , 1, [1] , ['Trail']),
-        "Short_H4_3" : ('US30' , 1/100 , 3000 , 5000 , 1, [1] , ['Trail']),
+        "Short_H4_2" : ('XAUUSD' , 1/100 , 30 , 50 , 1, [1] , ['ATR']),
+        "Short_H4_3" : ('US30' , 1/100 , 3000 , 5000 , 1, [1] , ['ATR']),
         "Short_H4_4" : ('XTIUSD' , 1/100 , 3000 , 5000 ,1 , [1] , ['ATR']) ,
-        "Short_H4_5" : ('US500' , 1/100 , 3000 , 5000 , 1 , [1] , ['Trail']) 
+        "Short_H4_5" : ('US500' , 1/100 , 3000 , 5000 , 1 , [1] , ['ATR']) 
     }
 
